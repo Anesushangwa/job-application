e3c6d0242c6c58e4a36979c9f77cec937cdf4d65
Anesushangwa <sha21032@byui.edu>
2023-03-27 08:58:20 +0200

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { JobApplicationFormComponent } from './job-application-form/job-application-form.component';
+import { AppliedJobsComponent } from './applied-jobs/applied-jobs.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes  = [
+  {path: '', component:   HomeComponent },
+  {path: 'applied-application', component:  AppliedJobsComponent },
+  {path: 'application-form', component:  JobApplicationFormComponent},
+];
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    JobApplicationFormComponent,
+    AppliedJobsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
